0ef713a8bc6720600670cfaf2001462546c9d49e
Alberto Tudela <ajtudela@gmail.com>
2024-04-01 10:26:18 +0200

    Update Copyright
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- scitos2_behavior_tree/include/scitos2_behavior_tree/condition/is_bumper_activated_condition.hpp
+++ scitos2_behavior_tree/include/scitos2_behavior_tree/condition/is_bumper_activated_condition.hpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2024 Alberto J. Tudela Roldán
+// Copyright (c) 2024 Grupo Avispa, DTE, Universidad de Málaga
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
